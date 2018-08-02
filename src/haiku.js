18ed2c4a45442f5a0990365283b54a596092a85e
Eric Shull and Scott Fraser <scott@>
2018-08-02 14:12:39 -0700

    washing method works

--- src/haiku.js
+++ src/haiku.js
@@ -36,6 +36,17 @@ export class Haiku {
       else if (line[i].includes('ey')) {
         line[i] = line[i].replace('ey', 'y')
       }
+      else if (line[i].includes('ay')) {
+        line[i] = line[i].replace('ay', 'y')
+        continue
+      }
+      else if (line[i].includes('es')) {
+        line[i] = line[i].replace('es', 's')
+      }
+      else if (line[i].includes('se') && !line[i].startsWith('s'))
+      {
+        line[i] = line[i].replace('se', 's')
+      }
     }
       return line
   }
@@ -51,10 +62,14 @@ export class Haiku {
         for(let x = 0 ; x < line[i].length ; x++) {
           if (line[i][x] == 'a') {
             vowelCount++
-            console.log(x);
           }
           if (line[i][x] == 'e') {
-            vowelCount++
+            if (line[i][x]-1 == 's' && line[i][x]-2 == 'a'){
+              continue
+            }
+            else{
+              vowelCount++
+            }
           }
           if (line[i][x] == 'i') {
             vowelCount++
@@ -70,33 +85,38 @@ export class Haiku {
           }
         }
       }
+    }
+    return vowelCount
   }
-  return vowelCount
-}
-
-cleanLines(line){
-  let newLine = this.removeSilentE(line)
-  newLine = this.removeSilentDouble(newLine)
-
-  return newLine
-}
 
 
-  // isHaiku(){
-  //
-  //   let line1 = this.cleanLines(this.line1)
-  //   let line2 = this.cleanLines(this.line2)
-  //   let line3 = this.cleanLines(this.line3)
-  //   console.log(line1);
-  //
-  //   let line1Count = this.countVowels(line1)
-  //   let line2Count = this.countVowels(line2)
-  //   let line3Count = this.countVowels(line3)
-  //
-  //   if (line1Count == 5 && line2Count == 7 && line3Count == 5){
-  //    return true
-  //  }
-  // }
+  cleanLines(line){
+    let newLine = this.removeSilentE(line)
+    newLine = this.removeSilentDouble(newLine)
+    newLine = this.removeSilentDouble(newLine)
+
+    return newLine
+  }
+
+  isHaiku(){
+
+    let line1 = this.cleanLines(this.line1)
+    let line2 = this.cleanLines(this.line2)
+    let line3 = this.cleanLines(this.line3)
+
+    console.log(line3);
+    let line1Count = this.countVowels(line1)
+    let line2Count = this.countVowels(line2)
+    let line3Count = this.countVowels(line3)
+
+    console.log(line3Count);
+
+    if (line1Count == 5 && line2Count == 7 && line3Count == 5){
+     return true
+   } else {
+     return false
+   }
+  }
 
 
 
